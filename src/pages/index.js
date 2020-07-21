afbf49b2fbe686726a503412e05c722715956e8b
mk1484 <flat.yz14@gmail.comm>
2020-07-22 03:59:13 +0900

    トップページの変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,52 +13,62 @@ export default () =>(
                 </div>
                 <nav className="nav">
                     <ul>
-                        <li><a href="base-index.html">TOP</a></li>
-                        <li><a href="base-about.html">ABOUT</a></li>
-                        <li><a href="base-works.html">WORKS</a></li>
+                        <li><a href="base-about.html">About</a></li>
+                        <li><a href="base-works.html">Works</a></li>
+                        <li><a href="base-works.html">Contact</a></li>
                     </ul>
                 </nav>
             </div>
         </header>
-        <section className="hero">
+        <section className="main">
+            <div className="container">
             <figure>
                 <img src="/images/main.jpg" alt="" />
             </figure>
-            <div className="catch">
-                <h1>texttexttext<br />texttexttext</h1>
-                <p>texttexttexttexttexttexttexttexttexttext</p>
-            </div>
-            <div className="wave">
-                <img src="images/wave.svg" alt="" />
             </div>
         </section>
-        <section className="service">
+        <section className="catch">
             <div className="container">
-                <h2 className="bar">About<span>About</span></h2>
+                <h1>Ishibashi</h1>
+                <p>web designer</p>
+            </div>
+        </section>
+        <section className="about">
+            <div className="container">
+                <h2>About</h2>
                 <div className="details">
                     <div className="detail">
                         <figure>
                             <img src="images/1.jpg" alt="" />
                         </figure>
-                        <h3>1</h3>
-                        <p>text</p>
-                        <p>texttexttexttext<br />texttexttexttexttexttext</p>
+                        <p>texttexttexttext<br />texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="works">
+            <div className="container">
+                <h2>Works</h2>
+                <div className="details">
+                    <div className="detail">
+                        <img src="images/1.jpg" alt="" />
                     </div>
                     <div className="detail">
-                        <figure>
-                            <img src="images/2.jpg" alt="" />
-                        </figure>
-                        <h3>2</h3>
-                        <p>text</p>
-                        <p>texttexttexttext<br />texttexttexttexttexttext</p>
+                        <img src="images/2.jpg" alt="" />
                     </div>
                     <div className="detail">
-                        <figure>
-                            <img src="images/3.jpg" alt="" />
-                        </figure>
-                        <h3>3</h3>
-                        <p>text</p>
-                        <p>texttexttexttext<br />texttexttexttexttexttext</p>
+                        <img src="images/3.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="details">
+                    <div className="detail">
+                        <img src="images/1.jpg" alt="" />
+                    </div>
+                    <div className="detail">
+                        <img src="images/2.jpg" alt="" />
+                    </div>
+                    <div className="detail">
+                        <img src="images/3.jpg" alt="" />
                     </div>
                 </div>
             </div>
