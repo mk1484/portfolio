a63124f73069fd3b4c493253c7438f93d60e961c
mk1484 <flat.yz14@gmail.comm>
2020-07-22 11:38:09 +0900

    ヘッダーのコンポーネント化

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default () => (
+    <header className="header">
+        <div className="container">
+            <div className="site">
+                <a href="base-index.html">
+                    <img src="images/logo.svg" alt="" />
+                </a>
+            </div>
+            <nav className="nav">
+                <ul>
+                    <li><a href="base-about.html">About</a></li>
+                    <li><a href="base-works.html">Works</a></li>
+                    <li><a href="base-works.html">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+)
