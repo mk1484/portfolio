460d3861f4404b8ce00c00f42d6e5862dad08f80
mk1484 <flat.yz14@gmail.com>
2020-07-22 11:39:59 +0900

    ヘッダーのコンポーネント化 (#17)
    Co-authored-by: mk1484 <flat.yz14@gmail.comm>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,12 @@
 import React from "react"
 
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 export default () =>(
     <div>
         <SEO />
-        <header className="header">
-            <div className="container">
-                <div className="site">
-                    <a href="base-index.html">
-                        <img src="images/logo.svg" alt="" />
-                    </a>
-                </div>
-                <nav className="nav">
-                    <ul>
-                        <li><a href="base-about.html">About</a></li>
-                        <li><a href="base-works.html">Works</a></li>
-                        <li><a href="base-works.html">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        <Header />
         <section className="main">
             <div className="container">
             <figure>
