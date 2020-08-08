2d7bc2139b9620d616606cee5062b1298ec5a8ea
mk1484 <flat.yz14@gmail.comm>
2020-08-08 23:10:07 +0900

    404ページのタイトルの指定

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 
 export default () => (
     <Layout>
-        <SEO />
+        <SEO pagetitle="ページが見つかりません" />
         <h1 style={{ padding: "20vh 0", textAlign: "center" }}>お探しのページが見つかりませんでした</h1>
     </Layout>
 )
