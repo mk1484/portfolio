ceff52bb3aee616db469bb812d307a5472900c3e
mk1484 <flat.yz14@gmail.comm>
2020-08-09 02:52:25 +0900

    404ページのURLを明示

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,9 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 
-export default () => (
+export default ({location}) => (
     <Layout>
-        <SEO pagetitle="ページが見つかりません" />
+        <SEO pagetitle="ページが見つかりません" pagepath={location.pathname} />
         <h1 style={{ padding: "20vh 0", textAlign: "center" }}>お探しのページが見つかりませんでした</h1>
     </Layout>
 )
