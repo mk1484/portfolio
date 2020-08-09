7ea979e250e3a606278c0378e7070bcb0e9a71c3
mk1484 <flat.yz14@gmail.comm>
2020-08-10 01:07:06 +0900

    works一覧のベース作成

---
+++ src/pages/works.js
@@ -0,0 +1,63 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+export default () =>(
+    <Layout>
+        <section className="content bloglist">
+            <div className="container">
+            <h1 className="bar">Works</h1>
+                <div className="posts">
+                    <article className="post">
+                        <a href="base-blogpost.html">
+                            <figure>
+                                <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+                            </figure>
+                            <h3>記事のタイトル</h3>
+                        </a>
+                    </article>
+                    <article className="post">
+                        <a href="base-blogpost.html">
+                            <figure>
+                                <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+                            </figure>
+                            <h3>記事のタイトル</h3>
+                        </a>
+                    </article>
+                    <article className="post">
+                        <a href="base-blogpost.html">
+                            <figure>
+                                <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+                            </figure>
+                            <h3>記事のタイトル</h3>
+                        </a>
+                    </article>
+                    <article className="post">
+                        <a href="base-blogpost.html">
+                            <figure>
+                                <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+                            </figure>
+                            <h3>記事のタイトル</h3>
+                        </a>
+                    </article>
+                    <article className="post">
+                        <a href="base-blogpost.html">
+                            <figure>
+                                <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+                            </figure>
+                            <h3>記事のタイトル</h3>
+                        </a>
+                    </article>
+                    <article className="post">
+                        <a href="base-blogpost.html">
+                            <figure>
+                                <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
+                            </figure>
+                            <h3>記事のタイトル</h3>
+                        </a>
+                    </article>
+                </div>
+            </div>
+        </section>
+    </Layout>
+)
