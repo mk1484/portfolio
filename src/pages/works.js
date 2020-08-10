e646e90e414a639f88d6460b26fa6984cb7547ec
mk1484 <flat.yz14@gmail.com>
2020-08-11 04:50:08 +0900

    works一覧ページの作成 (#53)
    * works一覧のベース作成
    
    * クエリの追加
    
    * 各記事のタイトルの表示
    
    * keyの追加
    
    * アイキャッチ画像の表示
    
    * アイキャッチ画像の高さを揃える
    
    * 画像の説明の追加
    
    * 記事へのリンクの設定
    
    * メタデータの設定
    
    Co-authored-by: mk1484 <flat.yz14@gmail.comm>

---
+++ src/pages/works.js
@@ -0,0 +1,57 @@
+import React from "react"
+
+import SEO from "../components/seo"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+
+export default ({ data, location }) => (
+    <Layout>
+        <SEO
+            pagetitle="works"
+            pagedesc="作品詳細"
+            pagepath={location.pathname}
+        />
+        <section className="content bloglist">
+            <div className="container">
+                <h1 className="bar">Works</h1>
+                <div className="posts">
+                    {data.allContentfulWorksPost.edges.map(({ node }) => (
+                    <article className="post" key={node.id}>
+                        <Link to={`/works/post/${node.slug}/`}>
+                            <figure>
+                                <Img
+                                    fluid={node.eyecatch.fluid}
+                                    alt={node.eyecatch.description}
+                                    style={{ height: "100%" }}
+                                />
+                            </figure>
+                            <h3>{node.title}</h3>
+                        </Link>
+                    </article>
+                    ))}
+                </div>
+            </div>
+        </section>
+    </Layout>
+)
+
+export const query = graphql`
+    query {
+      allContentfulWorksPost(sort: { order: DESC, fields: publishDate }) {
+        edges {
+          node {
+          title
+          id
+          slug
+            eyecatch {
+              fluid(maxWidth: 500) {
+              ...GatsbyContentfulFluid_withWebp
+              }
+              description
+            }
+          }
+        }
+      }
+    }
+`
