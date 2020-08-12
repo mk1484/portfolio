224a524ff83cc8d29841f001d87e615d3ab27eab
mk1484 <flat.yz14@gmail.com>
2020-08-13 01:36:10 +0900

    ナビゲーションに変更を加える (#63)
    * リンクの変更
    
    * ロゴのパス変更
    
    Co-authored-by: mk1484 <flat.yz14@gmail.comm>

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Logo from "../../public/images/logo.svg"
+
 export default () => (
     <header className="header">
         <div className="container">
             <div className="site">
                 <Link to={`/`}>
-                    <img src="images/logo.svg" alt="" />
+                    <img src={ Logo } alt="" />
                 </Link>
             </div>
             <nav className="nav">
                 <ul>
-                    <li><Link to={`/about`}>About</Link></li>
-                    <li><Link to={`/works`}>Works</Link></li>
-                    <li><Link to={`/contact`}>Contact</Link></li>
+                    <li><Link to={`/about/`}>About</Link></li>
+                    <li><Link to={`/works/`}>Works</Link></li>
+                    <li><Link to={`/contact/`}>Contact</Link></li>
                 </ul>
             </nav>
         </div>
