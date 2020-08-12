8e2b268420452b826e2d9be09ebd966188696e3a
mk1484 <flat.yz14@gmail.com>
2020-08-12 21:10:19 +0900

    worksページを複数ページに分けて生成 (#59)
    * リネーム・ファイルの移動
    
    * ページの生成
    
    * ページごとに記事を生成
    
    Co-authored-by: mk1484 <flat.yz14@gmail.comm>

--- src/templates/works-template.js
+++ src/templates/works-template.js
@@ -37,11 +37,11 @@ export default ({ data, location }) => (
 )
 
 export const query = graphql`
-    query {
+    query($skip: Int!, $limit: Int!) {
       allContentfulWorksPost(
       sort: { order: DESC, fields: publishDate }
-      skip: 0
-      limit: 6
+      skip: $skip
+      limit: $limit
       ) {
         edges {
           node {
