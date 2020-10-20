3f67c04f2b9c8ef3d88229d8bdfd72dd463b4867
mk1484 <flat.yz14@gmail.comm>
2020-10-20 16:54:41 +0900

    リンクの削除

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,6 @@ import { Link } from "gatsby"
 
 import Logo from "../../public/images/logo-w.svg"
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-    faTwitter,
-    // faFacebookSquare,
-    // faInstagram,
-} from "@fortawesome/free-brands-svg-icons"
-
 export default () => (
     <footer className="footer">
         <div className="container">
@@ -18,26 +11,6 @@ export default () => (
                     <img src={ Logo } />
                 </Link>
             </div>
-            {/*<ul className="sns">*/}
-                {/*<li>*/}
-                    {/*<a href="https://twitter.com/1484mi">*/}
-                        {/*<FontAwesomeIcon icon={faTwitter} />*/}
-                        {/*<span className="sr-only">Twitter</span>*/}
-                    {/*</a>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                    {/*<a href="https://facebook.com/">*/}
-                        {/*<FontAwesomeIcon icon={faFacebookSquare} />*/}
-                        {/*<span className="sr-only">Facebook</span>*/}
-                    {/*</a>*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                    {/*<a href="http://instagram.com/">*/}
-                        {/*<FontAwesomeIcon icon={faInstagram} />*/}
-                        {/*<span className="sr-only">Instagram</span>*/}
-                    {/*</a>*/}
-                {/*</li>*/}
-            {/*</ul>*/}
         </div>
     </footer>
 )
